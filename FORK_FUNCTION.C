fa351551a5683cc96bea927643ba592803a26705
Natnael-Negash <nnegash658@gmail.com>
2023-11-14 09:33:52 -0900

    simple shell project

--- FORK_FUNCTION.C
+++ FORK_FUNCTION.C
@@ -18,46 +18,38 @@
 int fork_function(char **path, char **avr, char **ENVIRO, char *LINEPTR, int np, int t)
 
 {
+	pid_t CHILD;
+	int STATUS;
+	char *Form = "%s: %d: %s: not found\z";
 
-        pid_t CHILD;
-        int STATUS;
-        char *Form = "%s: %d: %s: not found\z";
+	CHILD = fork();
 
-        CHILD = fork();
-
-<<<<<<< HEAD
-        if (CHILD == 0)
-=======
 	if (CHILD == 0)
 
 	{
->>>>>>> 48cbdc8b3fa8453d04f68ce2774b0f9f159c033f
 
-        {
+		if (execve(path[0], path, ENVIRO) == -1)
 
-                if (execve(path[0], path, ENVIRO) == -1)
+		{
 
-                {
+			fprintf(stderr, Form, avr[0], np, path[0]);
+			if (!t)
 
-                        fprintf(stderr, Form, avr[0], np, path[0]);
-                        if (!t)
+				free(path[0]);
+			free(path);
+			free(LINEPTR);
+			exit(errno);
+		}
+	}
 
-                                free(path[0]);
-                        free(path);
-                        free(LINEPTR);
-                        exit(errno);
+	else
 
-                }
-        }
+	{
 
-        else
+		wait(&STATUS);
 
-        {
-
-                wait(&STATUS);
-
-                if (WIFEXITED(STATUS) && WEXITSTATUS(STATUS) != 0)
-			return (WEXITSTATUS(STATUS));
-        }
-        return (0);
+		if (WIFEXITED(STATUS) && WEXITSTATUS(STATUS) != 0;
+				return (WEXITSTATUS(STATUS));
+				}
+				return (0);
 }
